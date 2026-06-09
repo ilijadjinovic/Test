6c7dfe0d0b9cd20e068423a70615139866ed9993
ilijadjinovic <ilija.djinovic@gmail.com>
2026-06-09 08:18:21 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,23 @@
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js';
-import { getAuth,GoogleAuthProvider,signInWithRedirect,signOut } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js';
+import { getAuth, GoogleAuthProvider, signInWithRedirect, signOut } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js';
 
-const firebaseConfig={
-apiKey: "AIzaSyCHdohv0tfLf8taODtLfMKQDPROJN_lwug",
-  authDomain: "rental-manager-da0fa.firebaseapp.com",
-  projectId: "rental-manager-da0fa",
-  storageBucket: "rental-manager-da0fa.firebasestorage.app",
+const firebaseConfig = {
+  apiKey:            "AIzaSyCHdohv0tfLf8taODtLfMKQDPROJN_lwug",
+  authDomain:        "rental-manager-da0fa.web.app",
+  projectId:         "rental-manager-da0fa",
+  storageBucket:     "rental-manager-da0fa.firebasestorage.app",
   messagingSenderId: "470999148120",
-  appId: "1:470999148120:web:2fde820f24ca194f2c4c2d"
+  appId:             "1:470999148120:web:2fde820f24ca194f2c4c2d"
 };
 
-const app=initializeApp(firebaseConfig);
+const app      = initializeApp(firebaseConfig);
 
-export const auth=getAuth(app);
-export const db=getFirestore(app);
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
 
-const provider=new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 
-export const login=()=>signInWithRedirect(auth,provider);
-export const logout=()=>signOut(auth);
+export const login  = () => signInWithRedirect(auth, provider);
+export const logout = () => signOut(auth);
 export const ADMIN_EMAIL = "ilija.djinovic@gmail.com";
